8b56de7923db13549006bd994c2e5ca55ecde765
Christopher Castillo <christopherhcastillo@gmail.com>
2021-04-21 11:32:24 -0400

    Basket Implementation
    Asynchronous Delete/Update/Add

--- dbcrstore/settings.py
+++ dbcrstore/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.context_processors.categories',
+                'basket.context_processors.basket',
             ],
         },
     },
